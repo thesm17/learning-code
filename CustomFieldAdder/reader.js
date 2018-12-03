ce654be7636383ce654fb3452d9caff7ac4b4c40
Smitty Penman <smitty.penman@sharpspring.com>
2018-12-03 16:54:02 -0500

    lots of work done on reader

--- CustomFieldAdder/reader.js
+++ CustomFieldAdder/reader.js
@@ -1,6 +1,7 @@
 //var poster = require('./poster');
 let fileUploaded = false;
 var fileContents;
+var radioLimit = 5, picklistLimit = 12;
 //var Papa = require('papaparse');
 
 //poster.postIt("614DF4BF4FEE0CE729F3484D40A0BA10","F21D9298D9DD0FCE331D5863D25F9B65",[{1:3}]);
@@ -19,36 +20,45 @@ const readUploadedFileAsText = (inputFile) => {
       resolve(csv);
     };
     temporaryFileReader.readAsText(inputFile);
-  })
-}
+  });
+};
 
 const handleFiles = async (files) => {
   const file = files[0];
-
+  var suggestArray = [], suggestions = [];
   try {
     fileContents = await readUploadedFileAsText(file);
-    var headers="";
-    for (i = 0; i<fileContents.data[0].length; i++) {
-      //this doesn't work yet. I'm trying to push the correct data type onto the end of each of the arrays.
-      //console.log(fileContents.data[1].push(determineDefaultFieldType(i)));
-      headers+=fileContents.data[0][i]+"<br>";
-    }
-    displayElement(headers);
+    rows = fileContents.data.length,  cols = fileContents.data[0].length,  vals = [];
 
-    determineDefaultFieldType(fileContents.data[2])
-    
-    //  fileContents.data.forEach(e => {displayElement(e)});
-    //displayRow(0);
+    //loop through all columns doing the following: count unique, determinine default, push that to array
+    for (i=0; i<cols;i++){
+      //works. pushes the type of field into the last row of matrix; should be changed from push to insert
+      suggestArray.push(determineDefaultFieldType(countUniqueVals(i)[0]));
+      //works. suggestions[] is full of sets
+      suggestions.push(countUniqueVals(i)[1]);
+      //trying to figure out how to fill an array iff there are a small number in the set using picklistLimit
+      suggestions[1].forEach
+      //works
+      fileContents.data.push(suggestArray);
+    }
+      console.log(suggestions);
+    //for Debugging
+    console.log(`Rows: ${rows}, columns: ${cols}, Column 0: ${vals}`);
+
+    var headers="";
+    for (i = 0; i<fileContents.data[i].length; i++) {
+      headers+=fileContents.data[i][0]+"<br>";
+    }
     
     
   } catch (e) {
     console.warn(e.message);
   }
-}
+};
 
 const displayElement = (e) => {
 document.getElementById("my-row").innerHTML += e;
-}
+};
 
 function errorHandler(evt) {
   if(evt.target.error.name == "NotReadableError") {
@@ -56,9 +66,18 @@ function errorHandler(evt) {
   }
 }
 
-const determineDefaultFieldType = (column) => {
-  var numValues = new Set(column).size;
-  if (numValues < 5) return 'radio';
-  else if (numValues >4 && numValues <12) return 'picklist';
+const countUniqueVals = (i) => {
+  var suggestions = new Set(fileContents.data.map(function(value, index) {return value[i];}));
+  var uniqueVals = suggestions.size-1;
+  return [uniqueVals,suggestions];
+  } 
+
+const determineDefaultFieldType = (numUnique) => {
+  if (numUnique < 5) return 'radio';
+  else if (numUnique >4 && numUnique <12) return 'picklist';
   else return 'text';
-}
+};
+
+const pushSuggestedFieldType = (newRow) => {
+  return fileContents.data.push(newRow);
+};
